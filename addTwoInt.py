253a20b23534c0a8bf70f2724e06b6178f2510af
edith <edithbalole@gmail.com>
2020-07-06 13:27:55 +0400

    ajout addTwolnt.py

---
+++ addTwoInt.py
@@ -0,0 +1,13 @@
+def add():
+
+    a= int(input("veuillez inserer la premiere valeur:"))
+    b= int(input("veuillez inserer la premiere valeur:"))
+    print(a+b)
+
+def main():
+    add()
+
+if __name__ == "__main__":
+    main()
+
+
